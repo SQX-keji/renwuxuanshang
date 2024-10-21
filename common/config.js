85d962ebd5ba147312fe35fefd50d92316bee30c
于洋 <604589552@qq.com>
2024-10-21 17:24:42 +0800

    first commit

---
+++ common/config.js
@@ -0,0 +1,7 @@
+const ROOTPATH1 = "https://renwu.xiansqx.com/sqx_fast";//后台服务域名
+const ROOTPATH = "https://taobao.xianmxkj.com";//商品库域名还有其他代理域名
+module.exports = {
+    APIHOST: ROOTPATH,
+    APIHOST1: ROOTPATH1,
+    ROOTPATH: ROOTPATH
+};
